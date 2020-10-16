f49c49fc873600b05f7f555f2f14484cbdcec682
KUMAR SATYAM <53793443+satyam0500@users.noreply.github.com>
2020-10-16 13:24:11 +0530

    Update script.js

--- project2/script.js
+++ project2/script.js
@@ -1,4 +1,5 @@
 var audio = document.createElement('audio');
+//you can download any sound you want
 audio.setAttribute('src','sound.mp3');
+//to make the sound effect audio.loop is used
 audio.loop=true;
-//audio.play();
